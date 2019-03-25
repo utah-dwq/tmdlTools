39dd63ffa870aa69b71ef8daeed1ea79ae45d30f
Elise Hinman <43010657+edhinman@users.noreply.github.com>
2019-03-25 15:50:33 -0600

    created file upload functionality
    - upload file, run tmdlTools, review data and inputs.

--- inst/TMDLApp/app.R
+++ inst/TMDLApp/app.R
@@ -7,27 +7,6 @@ require(yarrr)
 require(colorspace)
 require(reshape2)
 require(markdown)
-# wb_path <- "C:\\Users\\ehinman\\Documents\\GitHub\\ecoli_tmdl\\Fremont_data_2019-02-22.xlsx"
-# wb.dat <- openxlsx::loadWorkbook(wb_path)
-# ecoli.dat <- openxlsx::readWorkbook(wb.dat,sheet="Daily_Geomean_Data",startRow=1)
-# ecoli.dat$Date <- as.Date(ecoli.dat$Date, origin="1899-12-30")
-# if("Flow_data"%in%wb.dat$sheet_names){
-#   flow.dat <- readWorkbook(wb.dat, sheet="Flow_data", startRow=1)
-#   flow.dat$Date <- as.Date(flow.dat$Date, origin="1899-12-30")
-# }
-# if("LDC_Data"%in%wb.dat$sheet_names){
-#   loading.dat <- openxlsx::readWorkbook(wb.dat, sheet="LDC_Data")
-#   loading.dat$Date <- as.Date(loading.dat$Date, origin="1899-12-30")
-# }else{loading.dat=NULL}
-# month.dat <- openxlsx::readWorkbook(wb.dat,sheet="Monthly_Data",startRow=1)
-# rec.dat <- openxlsx::readWorkbook(wb.dat,sheet="Rec_Season_Data",startRow=1)
-# irg.dat <- openxlsx::readWorkbook(wb.dat,sheet="Irg_Season_Data",startRow=1)
-# specs <- openxlsx::readWorkbook(wb.dat,sheet="Inputs",startRow=1)
-# 
-# max_crit = specs$Value[specs$Parameter=="Max Criterion"]
-# geom_crit = specs$Value[specs$Parameter=="Geometric Mean Criterion"]
-# cf = specs$Value[specs$Parameter=="Correction Factor"]
-# mos = specs$Value[specs$Parameter=="Margin of Safety"]
 
 perc.red <- function(x,y){100-x/y*100}
 
@@ -35,19 +14,20 @@ perc.red <- function(x,y){100-x/y*100}
 ui <- fluidPage(title="E.coli Data Explorer",
                 titlePanel(title=div(img(width="8%",height="8%",src="dwq_logo_small.png"), em("Escherichia coli"),"Data Visualization Tool")),
                 tabsetPanel(id="all_the_things",
-                            tabPanel("User Guide",
-                                     includeMarkdown("C:\\Users\\ehinman\\Documents\\GitHub\\tmdlTools\\user_guide\\user_guide.Rmd")),
                             tabPanel("Upload Data",
+                                     useShinyjs(),
                                      h3("Select your Excel workbook containing E.coli data"),
                                      sidebarPanel(fileInput("workbook","Select Workbook"),
-                                                  uiOutput("tmdltool")),
+                                                  uiOutput("tmdltool"),
+                                                  uiOutput("selectsheet")),
                                      mainPanel(
                                        tabsetPanel(
-                                         tabPanel("Parameter Data", DTOutput("parameterdat"), style= "font-size:75%"),
-                                         tabPanel("Flow Data", DTOutput("flowdat"), style= "font-size:75%")
+                                         tabPanel("Data View", DTOutput("datview"), style= "font-size:75%"),
+                                         tabPanel("Check Inputs", DTOutput("inputdat"), style= "font-size:75%")
                                        )
                                      )),
-                            conditionalPanel(condition = "input.workbook")
+                            tabPanel("User Guide",
+                                     includeMarkdown("C:\\Users\\ehinman\\Documents\\GitHub\\tmdlTools\\user_guide\\user_guide.Rmd"))
               )
 )
 
@@ -60,49 +40,77 @@ server <- function(input, output) {
   observeEvent(input$workbook,{
     fileup = input$workbook
     workbook$wb_path = fileup$datapath})
-    
+ 
+# Run tmdl tools widget, which disables once clicked   
  output$tmdltool <- renderUI({
     req(input$workbook)
     radioButtons("tmdltool", label = "Run tmdlTools?", selected = character(0), choices=c("Yes","No"), inline=TRUE)
   })
+ 
+ observeEvent(input$tmdltool,{
+   disable("tmdltool")
+ })
+
+# Create drop down menu of sheets contained in xlsx file
+
+ output$selectsheet <- renderUI({
+   req(workbook$inputs)
+   selectInput("selectsheet", label = "Select sheet to view.", selected = NULL, choices=c("Raw Concentrations"="ecoli","Flow"="flow",
+                                                                                          "Daily Geomean Concentrations" = "dailyvalues", "Loadings"="ldc",
+                                                                                          "Monthly Geomeans" = "month", "Rec/Non-Rec Geomeans"= "rec",
+                                                                                          "Irrigation/Non-Irrigation Geomeans"="irg"))
+ })
+ 
+### Reading in the data from the file to the reactive environment ### 
  observe({
+   req(input$tmdltool)
    if(input$tmdltool=="Yes"){
      out <- tmdlTools::tmdlCalcs(workbook$wb_path, overwrite = FALSE)
    }else{
-       out = openxlsx::loadWorkbook(workbook$wb_path)
-     }
+       dat = openxlsx::loadWorkbook(workbook$wb_path)
+       sheets = dat$sheet_names[!dat$sheet_names=="READ ME"]
+       out <- lapply(sheets, function(x)openxlsx::readWorkbook(workbook$wb_path, sheet = x, detectDates = TRUE))
+       names(out) = sheets
+   }
+   convertds = out$Inputs$Value[out$Inputs$Parameter=="Rec Season Start"|out$Inputs$Parameter=="Rec Season End"|out$Inputs$Parameter=="Irrigation Season Start"|out$Inputs$Parameter=="Irrigation Season End"]
+   convertds1 = as.Date(convertds, origin = "1899-12-30")
+   convertds1 = format(convertds1, "%b %d")
+   out$Inputs[out$Inputs$Value%in%convertds,"Value"] = convertds1
+   workbook$ecoli = out$Ecoli_data
+   workbook$dailyvalues = out$Daily_Geomean_Data
+   workbook$month = out$Monthly_Data
+   workbook$rec = out$Rec_Season_Data
+   workbook$irg = out$Irg_Season_Data
+   workbook$inputs = out$Inputs 
+   
+   if(!is.null(out$Flow_data)){
+     workbook$flow = out$Flow_data
+     workbook$ldc = out$LDC_Data
+   }
  })
-    # Run through TMDLcalcs
-    
-    exceeds <- ifelse(out$ecoli$E.coli_Geomean>out$max_crit, 1, 0)
-    workbook$ecoli = out$ecoli
-    workbook$exceeds = exceeds
-    if(!is.null(out$flow)){
-      workbook$flow = out$flow
-      workbook$ldc = out$ldc 
-    }
-    # Add list items from out to reactive values object
-    workbook$month = out$month
-    workbook$rec = out$rec
-    workbook$irg = out$irg
-    workbook$max_crit = out$max_crit
-    workbook$geom_crit = out$geom_crit
-    workbook$cf = out$cf
-    workbook$mos = out$mos
-  })
+ 
+ observe({
+   req(input$selectsheet)
+   tableview = isolate(workbook[[input$selectsheet]])
+   
+   # Load data tables on first page
+   output$datview <- renderDT(tableview, 
+                              rownames = FALSE,
+                              options = list(dom="ft", paging = FALSE, scrollX=TRUE, scrollY = "300px"))
+   
+ })
 
-# Load data tables on first page
-  output$parameterdat <- renderDT(workbook$ecoli, 
-                                  rownames = FALSE,
-                                  options = list(dom="ft", paging = FALSE, scrollX=TRUE, scrollY = "300px"))
-  output$flowdat <- renderDT(workbook$flow,
+  output$inputdat <- renderDT(workbook$inputs,
                              rownames = FALSE, 
                              options = list(dom="ft", paging = FALSE, scrollX=TRUE, scrollY = "300px"))
-  
-  
-  
  
 }
 
 # Run the application
 shinyApp(ui = ui, server = server)
+
+#### UNUSED CODE ####
+# out$geom_crit = out$Inputs[out$Inputs$Parameter=="Geometric Mean Criterion","Value"]
+# out$max_crit = out$Inputs[out$Inputs$Parameter=="Max Criterion","Value"]
+# out$cf = out$Inputs[out$Inputs$Parameter=="Correction Factor","Value"]
+# out$mos = out$Inputs[out$Inputs$Parameter=="Margin of Safety","Value"]
