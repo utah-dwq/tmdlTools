62cb6ba80ba744743b593487bd5104229a1912de
Elise Hinman <43010657+edhinman@users.noreply.github.com>
2019-04-09 12:00:03 -0600

    Update app.R

--- inst/TMDLApp/app.R
+++ inst/TMDLApp/app.R
@@ -116,7 +116,7 @@ output$dwnloadbutton <- renderUI({
  observe({
    req(input$tmdltool)
    if(input$tmdltool=="Yes"){
-     out <- tmdlTools::tmdlCalcs(workbook$wb_path, exportfromfunc = FALSE)
+     out <- tmdlCalcs(workbook$wb_path, exportfromfunc = FALSE)
    }else{
        dat = openxlsx::loadWorkbook(workbook$wb_path)
        sheets = dat$sheet_names[!dat$sheet_names=="READ ME"]
